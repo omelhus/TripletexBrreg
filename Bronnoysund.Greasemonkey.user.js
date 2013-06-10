4be314dee254e8001ad48ec141a2481016b422db
Ole Melhus <ole@on-it.no>
2013-06-10 15:07:18 +0200

    Updated Greasemonkey as well

--- Bronnoysund.Greasemonkey.user.js
+++ Bronnoysund.Greasemonkey.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name       Tripletex Brønnøysund (Greasemonkey)
 // @namespace  http://github.com/omelhus
-// @version    1.6
+// @version    2.1
 // @description  Hent firmainformasjon fra Brønnøysund i Tripletex. Søk på navn eller bruk shift + enter i orgnr for å hente informasjon.
 // @match       https://tripletex.no/execute/*
 // @match       https://tripletex.no/contentBlank*
 // @copyright  2013+, Ole Melhus
 // ==/UserScript==
 
-$.getScript("https://github.com/omelhus/TripletexBrreg/raw/master/Bronnoysund.user.js");
+$.getScript("https://github.com/omelhus/TripletexBrreg/raw/master/Bronnoysund.user.js?v=2.1");
