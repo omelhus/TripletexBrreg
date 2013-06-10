c6d2d823d518b69fdf9eb15ef765d4dc1c40bf4b
Ole Melhus <ole@on-it.no>
2013-06-10 15:26:45 +0200

    Fixed it to sandboxmode in FF

--- Bronnoysund.user.js
+++ Bronnoysund.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name		Tripletex Brønnøysund
 // @namespace	http://github.com/omelhus
-// @version		2.1
+// @version		2.2
 // @description	Hent firmainformasjon fra Brønnøysund i Tripletex. Søk på navn eller bruk shift + enter i orgnr for å hente informasjon.
 // @match       https://tripletex.no/execute/*
 // @match       https://tripletex.no/contentBlank*
-// @grant 		none
+// @grant 		GM_Log
 // @copyright	2013+, Ole Melhus
 // @require		http://code.jquery.com/jquery-2.0.2.min.js
 // @require		http://code.jquery.com/ui/1.10.3/jquery-ui.js
